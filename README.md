e322bbdb3bf7e1dfa64d6bf3442ea9ac4c317031
Diego WS <diego.wesllen.silva@gmail.com>
2020-05-24 00:00:12 -0300

    Setting output directory

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # funds_n_indexes
 Gera um dataframe com a cota de fundos selecionados e a cotação de alguns índices no dia.
+
+## Como utilizar?
+
+Instale todas os pacotes presentes no `code/set_packages.R`.
+
+Execute o código `code/main_data_spawner.R`.
+
+O resultado estará na pasta `data`.
--- code/main_data_spawner.R
+++ code/main_data_spawner.R
@@ -4,7 +4,7 @@ source("code/set_packages.R")
 source("code/get_index.R")
 source("code/get_fund.R")
 
-ini_date = '19/05/2020'
+ini_date = '18/05/2020'
 end_date = '20/05/2020'
 
 cnpj_list = get_funds_at_least(1e3,1e9)
@@ -15,3 +15,5 @@ df_index = spawn_index_dataframe(ini_date,end_date)
 df_joined = merge(df_funds
                   ,df_index
                   ,by = "date")
+
+write.csv(df_joined,file=paste0("data/output_",Sys.time() %>% gsub("[^0-9 ]","",.) %>% gsub(" ","_",.),".csv"),row.names = F)
